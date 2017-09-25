da707287e34ca683e9f218bf40e36993cf9bcbcc
Simona Colapicchioni <s.colapicchioni@gmail.com>
2017-09-25 11:22:56 +0200

    Ask The User The Name And Print a Greeting Message

--- H01_Variables/Program.cs
+++ H01_Variables/Program.cs
@@ -8,8 +8,42 @@ namespace H01_Variables
 {
     class Program
     {
+        //method
+        //entry point
+        
+        /*
+          this is 
+          a multiline
+          comment
+        */
         static void Main(string[] args)
         {
+            //userName = ""; too early!!
+
+            Console.WriteLine("Hello World!");
+            Console.Write("What's your name? ");
+
+            //1)variable declaration
+            // type name;
+            string userName;
+
+            //2) assignment
+            // name = expression;
+            // target <= value
+
+            userName = Console.ReadLine();
+
+            //string message;
+            //message = "Hello " + userName;
+            //Console.WriteLine(message);
+
+            Console.WriteLine("Hello " + userName);
+
+
+            //Console.WriteLine("Hello " + Console.ReadLine());
+
+            Console.ReadLine();
+            //Console.ReadLine() = userName; NO!
         }
     }
 }
