5c060ed7aaa9f05dba9435e8c37c079f0bf492b1
Simona Colapicchioni <s.colapicchioni@gmail.com>
2017-09-26 08:55:21 +0200

    Lab Variables 6 7

---
+++ H01_LabVariables06/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace H01_LabVariables06
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+            Vraag op een nummer en print "True" 
+            als het nummer een meervoud van 3 is
+            */
+
+        }
+    }
+}
---
+++ H01_LabVariables07/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace H01_LabVariables07
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+             Vraag op de naam van de user, dan print
+             - hoe lang het is
+             - de eerste letter van de naam
+             - de laatste letter van de naam
+            */
+        }
+    }
+}
