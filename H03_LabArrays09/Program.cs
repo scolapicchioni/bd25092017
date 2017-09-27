641452301f5e861020d936bbfc39fe020f64319a
Simona Colapicchioni <s.colapicchioni@gmail.com>
2017-09-27 10:29:15 +0200

    Arrays

---
+++ H03_LabArrays09/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace H03_LabArrays09 {
+    class Program {
+        static void Main(string[] args) {
+            /*
+             * Write a programin C# Sharp to separate odd and even integers in separate arrays. 
+Test Data : 
+Input the number of elements to be stored in the array :5 
+Input 5 elements in the array : 
+element - 0 : 25 
+element - 1 : 47 
+element - 2 : 42 
+element - 3 : 56 
+element - 4 : 32 
+Expected Output: 
+The Even elements are: 
+42 56 32 
+The Odd elements are : 
+25 47
+*/
+        }
+    }
+}
