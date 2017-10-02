783f4c500569949e0d1b6a04508027eb9677837a
Simona Colapicchioni <s.colapicchioni@gmail.com>
2017-10-02 12:37:37 +0200

    PhoneScenario01

---
+++ H04_OO05/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace H04_OO05 {
+    class Program {
+        static void Main(string[] args) {
+            /*
+             * Take two Phones to the Tech Support 
+             * to copy the Contacts from one phone to the other
+             */
+        }
+    }
+}
