18ae0da5c37c1b41b5c00964b4b0ea5c30f884bf
Simona Colapicchioni <s.colapicchioni@gmail.com>
2017-10-02 15:53:18 +0200

    Phone

--- H04_OO05/TechSupport.cs
+++ H04_OO05/TechSupport.cs
@@ -9,7 +9,7 @@ namespace H04_OO05 {
         public void CopyContacts(Phone source, Phone target) {
             for (int i = 0; i < source.ContactsCount; i++) {
                 Contact sourceContact = source.GetContactAt(i);
-                if (sourceContact != null)
+                //if (sourceContact != null)
                     //target.AddContact(new Contact() {
                     //    Name = sourceContact.Name,
                     //    PhoneNumber = sourceContact.PhoneNumber
