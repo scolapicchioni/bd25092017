0819c64d48e9f2c54fdf4bc81521b4d901a82f97
Simona Colapicchioni <s.colapicchioni@gmail.com>
2017-11-09 12:42:54 +0100

    event tests

--- H08_Events01.Tests/UnitTest1.cs
+++ H08_Events01.Tests/UnitTest1.cs
@@ -6,19 +6,26 @@ namespace H08_Events01.Tests
     [TestClass]
     public class CarTests
     {
+
         [TestMethod]
         public void GivenANewCar_WhenColorChanges_ThenAnEventShouldBeRaisedWithTheNewColor()
         {
+            //GivenANewCar
             Car c = new Car();
+            ConsoleColor actual = ConsoleColor.Black;
 
-            ConsoleColor actual=ConsoleColor.Black;
-
-            c.ColorChanged += (color) =>
+            c.ColorChanged += color => {
                 actual = color;
-            ;
+            };
+            
 
-            c.Color = ConsoleColor.Blue;
-            Assert.AreEqual(ConsoleColor.Blue, actual);
+            //WhenColorChanges
+            c.Color = ConsoleColor.Red;
+
+            //ThenAnEventShouldBeRaisedWithTheNewColor
+            Assert.AreEqual(ConsoleColor.Red, actual);
         }
+        
+
     }
 }
